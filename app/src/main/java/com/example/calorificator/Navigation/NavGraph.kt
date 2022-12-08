9ff5603da471af02a2e28ecf4d0ccc2704c54e82
iMshivam25 <ahivam.mishra.alld@gmail.com>
2022-12-08 14:03:01 +0530

    splash screen

---
+++ app/src/main/java/com/example/calorificator/Navigation/NavGraph.kt
@@ -0,0 +1,31 @@
+package com.example.calorificator.Navigation
+
+import android.window.SplashScreen
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.calorificator.HomeScreen
+import com.example.calorificator.SplashScreen
+
+@Composable
+fun SetupNavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Splash.route
+    ) {
+        composable(route = Screen.Splash.route) {
+            SplashScreen(navController = navController)
+        }
+        composable(route = Screen.Home.route) {
+            HomeScreen()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/calorificator/SplashScreen.kt
@@ -0,0 +1,40 @@
+package com.example.calorificator
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.navigation.NavHostController
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.example.calorificator.Navigation.Screen
+import com.example.calorificator.ui.theme.splashcolor
+
+@Composable
+fun SplashScreen(navController: NavHostController) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(splashcolor)
+    ) {
+        val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.logo3))
+        val logoAnimationState =
+            animateLottieCompositionAsState(composition = composition)
+        LottieAnimation(
+            composition = composition,
+            progress = { logoAnimationState.progress }
+        )
+        if (logoAnimationState.isAtEnd && logoAnimationState.isPlaying) {
+            navController.navigate(Screen.Home.route)
+        }
+    }
+}
