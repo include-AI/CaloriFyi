85baf3a83b0cca6cb09ff093733324096c9966ee
anupamtudu <atudu760@gmail.com>
2023-03-16 23:10:54 +0530

    Onboarding Screen Images Update

--- app/src/main/java/com/example/calorifyi/Navigation/OnboardingItems.kt
+++ app/src/main/java/com/example/calorifyi/Navigation/OnboardingItems.kt
@@ -13,9 +13,9 @@ class OnboardingItems {
         companion object{
             fun getData(): List<OnBoardingItems>{
                 return listOf(
-                    OnBoardingItems(R.drawable.os1, R.string.OnboardingTitle1, R.string.Desc1),
-                    OnBoardingItems(R.drawable.os2, R.string.OnboardingTitle2, R.string.Desc2),
-                    OnBoardingItems(R.drawable.os3, R.string.OnboardingTitle3, R.string.Desc3)
+                    OnBoardingItems(R.drawable.os_1, R.string.OnboardingTitle1, R.string.Desc1),
+                    OnBoardingItems(R.drawable.os_2, R.string.OnboardingTitle2, R.string.Desc2),
+                    OnBoardingItems(R.drawable.os_3, R.string.OnboardingTitle3, R.string.Desc3)
                 )
             }
         }
